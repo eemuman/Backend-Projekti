df56e1fab169754f6304e9acd864ece02966ad28
Eemil V <eemilv11@gmail.com>
2021-12-27 17:27:15 +0200

    Main.js fixaus

--- frontend/quiz-front/src/Pages/Main.js
+++ frontend/quiz-front/src/Pages/Main.js
@@ -4,14 +4,11 @@ import Container from "@mui/material/Container";
 import Settings from "../Components/Settings";
 
 export default function Main() {
-  const [langs, setLangs] = useState(["Test", "Test2", "Test3", "Test4"]);
-  const [amountofWords, setAmountofWords] = useState(0);
-
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <Settings langs={langs} setAmountofWords={setAmountofWords} />
+        <Settings />
       </Container>
     </React.Fragment>
   );
