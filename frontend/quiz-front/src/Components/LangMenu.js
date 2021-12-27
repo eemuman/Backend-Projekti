3a72438e6db491be9d2c6dab70b099ac6b715920
Eemil V <eemilv11@gmail.com>
2021-12-27 15:50:47 +0200

    Pieniä muokkauksia kielenvalintaan

--- frontend/quiz-front/src/Components/LangMenu.js
+++ frontend/quiz-front/src/Components/LangMenu.js
@@ -1,34 +1,43 @@
 import React from "react";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import FormHelperText from "@mui/material/FormHelperText";
 
 export default function LangMenu(props) {
   return (
     <div>
-      <Select
-        labelId={props.labelId}
-        id={props.id}
-        value={props.curLang}
-        label={props.label}
-        onChange={props.handle}
-      >
-        {" "}
-        {props.langs.map((lang, index) => {
-          if (lang !== props.secondLang) {
-            return (
-              <MenuItem key={index} value={lang}>
-                {lang}
-              </MenuItem>
-            );
-          } else {
-            return (
-              <MenuItem disabled key={index} value={lang}>
-                {lang}
-              </MenuItem>
-            );
-          }
-        })}
-      </Select>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id={props.label}>{props.label}</InputLabel>
+
+        <Select
+          labelId={props.labelId}
+          id={props.id}
+          value={props.curLang}
+          label={props.label}
+          onChange={props.handle}
+          autoWidth
+        >
+          {" "}
+          {props.langs.map((lang, index) => {
+            if (lang !== props.secondLang) {
+              return (
+                <MenuItem key={index} value={lang}>
+                  {lang}
+                </MenuItem>
+              );
+            } else {
+              return (
+                <MenuItem disabled key={index} value={lang}>
+                  {lang}
+                </MenuItem>
+              );
+            }
+          })}
+        </Select>
+        <FormHelperText>{props.helperTxt}</FormHelperText>
+      </FormControl>
     </div>
   );
 }
