bd581fb55a80f7882aaeedebded26e8cb667efb8
Eemil V <eemilv11@gmail.com>
2021-12-30 19:00:15 +0200

    Teemojen haku app.get Index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const vocab = require("./routes/Vocab");
 const app = vocab.connection;
+
+app.get("/themes", async (req, res) => {
+  try {
+    const themes = await vocab.getThemes();
+    res.send(themes);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
