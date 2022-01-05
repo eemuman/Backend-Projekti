e058aadaaa34573efb99b75932c53cd570c4d7bb
Eemil V <eemilv11@gmail.com>
2022-01-05 12:18:50 +0200

    Modal käyttämään propseista annettua elementtiä, jotta voidaan käyttää samaa pohjaa eri funktionaalisuudelle

--- frontend/quiz-front/src/Components/ModalBase.js
+++ frontend/quiz-front/src/Components/ModalBase.js
@@ -42,9 +42,7 @@ export default function TransitionsModal(props) {
             <Typography id="transition-modal-title" variant="h6" component="h2">
               {props.title}
             </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            {props.form}
           </Box>
         </Fade>
       </Modal>
