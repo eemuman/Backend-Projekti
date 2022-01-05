8375aec99070f37afa046d360cb33f3f75595454
Eemil V <eemilv11@gmail.com>
2022-01-05 12:19:19 +0200

    Pohjaa uuden sanan luomiselle

--- frontend/quiz-front/src/Components/Words.js
+++ frontend/quiz-front/src/Components/Words.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ModalBase from "./ModalBase";
+import NewWord from "./NewWord";
 
 export default function Words() {
   console.log("COMING HERE! W");
   return (
     <div>
-      <ModalBase btnTitle="Luo Sana" title="Luo uusi Sana" />
+      <ModalBase btnTitle="Luo Sana" title="Luo uusi Sana" form={<NewWord />} />
     </div>
   );
 }
