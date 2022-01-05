180a837a120b1d57b0f2ed14b1edfdea68c10d5b
Eemil V <eemilv11@gmail.com>
2022-01-05 12:19:05 +0200

    Pohja uuden sanan luomiselle

---
+++ frontend/quiz-front/src/Components/NewWord.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function NewWord() {
+  return (
+    <Box component="form">
+      <TextField id="TEST" label="Standard" variant="standard" />
+    </Box>
+  );
+}
