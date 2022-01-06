1c32bb800ecc5be0fa75d2b2547282ca78287c16
Eemil V <eemilv11@gmail.com>
2022-01-06 11:52:10 +0200

    Tehty pieni muokkaus, jotta jos ei olla vielä ladattu sanoja databasesta, niin ohjelma ei hajoa käsiin

--- frontend/quiz-front/src/Components/Words.js
+++ frontend/quiz-front/src/Components/Words.js
@@ -1,16 +1,21 @@
 import React from "react";
+import DisplayWords from "./DisplayWords";
 import ModalBase from "./ModalBase";
 import NewWord from "./NewWord";
 
 export default function Words(props) {
-  console.log("COMING HERE! W");
-  return (
+  return props.allWords.length === 0 ||
+    props.langs.length === 0 ||
+    props.themes.length === 0 ? (
+    <div>Loading..</div>
+  ) : (
     <div>
       <ModalBase
         btnTitle="LUO UUSI SANA"
         title="LUO UUSI SANA"
         form={<NewWord langs={props.langs} themes={props.themes} />}
       />
+      <DisplayWords langs={props.langs} words={props.allWords}></DisplayWords>
     </div>
   );
 }
