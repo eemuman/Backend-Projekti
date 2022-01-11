d2403ce6ddb39d29218356407f5b2057a9e1bbbd
Eemil V <eemilv11@gmail.com>
2022-01-11 15:41:44 +0200

    Pohjaa autentisoinnin frontendille

--- frontend/quiz-front/src/App.js
+++ frontend/quiz-front/src/App.js
@@ -1,16 +1,31 @@
 import Main from "./Pages/Main";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Admin from "./Pages/Admin";
+import SignIn from "./Pages/SignIn";
+import React, { useState } from "react";
 
 function App() {
-  const dev = true;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const RequireAuth = ({ children, redirectTo }) => {
+    console.log(isLoggedIn);
+    return isLoggedIn ? children : <Navigate to={redirectTo} />;
+  };
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/SignIn" element={<SignIn />} />
         <Route path="/Play" element={<Main />} />
-        <Route path="/Admin" element={<Admin />} />
+
+        <Route
+          path="/Admin"
+          element={
+            <RequireAuth redirectTo="/SignIn">
+              <Admin />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/quiz-front/src/Pages/SignIn.js
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          KIRJAUDU SISÄÄN
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="username"
+            label="Käyttäjänimi"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Salasana"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
