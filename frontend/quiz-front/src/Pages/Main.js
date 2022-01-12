1112dc3eb204f65b4fb95caa89f7ec21e30b08f8
Eemil V <eemilv11@gmail.com>
2022-01-12 17:13:48 +0200

    Annetaan fetchData myös Play puolelle, jotta voidaan hakea sanat uudestaan

--- frontend/quiz-front/src/Pages/Main.js
+++ frontend/quiz-front/src/Pages/Main.js
@@ -116,6 +116,7 @@ export default function Main() {
             secondaryLang={secondaryLang}
             curTheme={curTheme}
             setIsPlaying={resetAll}
+            fetchData={fetchData}
           ></Play>
         )}
       </Container>
